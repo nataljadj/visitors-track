f6e0b39fd412e82835712af4945cf7279af18572
Nataliia Diedyk <nataliia.diedyk2@cgm.com>
2016-10-20 01:31:25 +0300

    Read data from son for dashboard items, add general styles

--- src/app/home/dashboard.service.ts
+++ src/app/home/dashboard.service.ts
@@ -1,31 +1,37 @@
 import { Injectable } from '@angular/core';
-/* ------- !Angular 2 native components  ---------*/
-
-import { UtilityService } from 'utilService';
-/* ------- !Services  ---------*/
-
-import { Config } from 'appConfig';
-/* ------- !Config  ---------*/
+import { Http } from '@angular/http';
+import { Observable }     from 'rxjs/Observable';
+import 'rxjs/Rx';
 
 @Injectable()
-export class DashboardService {
-  private liveUpdate;
 
-  constructor(
-    private utilityService: UtilityService,
-    private config: Config) {
+export class DashboardService {
+    private DASHBOARD_URL = '../../assets/mock-data/routes_data.json';
+    private liveUpdate;
+
+  constructor(private http:Http) {
   }
 
   getDashboardItems() {
-    return this.utilityService.generateGetHttp(this.config.getUrlsGroup().ROUTES_DATA_URL)
-               .map((res: Object) => res.homeNav);
+    return this.http.get(this.DASHBOARD_URL)
+        .map(res => res.json())
+        .catch(this.handleError);
   }
 
   liveDataUpdate(items: Array[]) {
-    return this.utilityService.generateGetHttp(items.liveUpdate)
+    return this.http.get(items.liveUpdate)
                .map((res: Object) => new Object({
                  label : items.label,
                  count : res.count
                }));
   }
+
+    private handleError (error: any) {
+        // In a real world app, we might use a remote logging infrastructure
+        // We'd also dig deeper into the error to get a better message
+        let errMsg = (error.message) ? error.message :
+            error.status ? `${error.status} - ${error.statusText}` : 'Server error';
+        console.error(errMsg); // log to console instead
+        return Observable.throw(errMsg);
+    }
 }
--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,39 +1,37 @@
 import { Component } from '@angular/core';
 
-import { AppState } from '../app.service';
-import { Title } from './title';
-import { XLarge } from './x-large';
+import { DashboardService } from './dashboard.service';
 
 @Component({
-  // The selector is what angular internally uses
-  // for `document.querySelectorAll(selector)` in our index.html
-  // where, in this case, selector is the string 'home'
-  selector: 'home',  // <home></home>
-  // We need to tell Angular's Dependency Injection which providers are in our app.
+  selector: 'home',
   providers: [
-    Title
+      DashboardService
   ],
-  // Our list of styles in our component. We may add more to compose many styles together
   styleUrls: [ './home.style.css' ],
-  // Every Angular template is first compiled by the browser before Angular runs it's compiler
   templateUrl: './home.template.html'
 })
 export class Home {
-  // Set our default values
-  localState = { value: '' };
-  // TypeScript public modifiers
-  constructor(public appState: AppState, public title: Title) {
+ public items;
+  public counts;
 
+  constructor(private _dashboardService: DashboardService) {
+    this.items = [];
+    this.counts = [];
+  }
+
+  // just for testing, implement function, that will fetch data from server, when it will be ready
+  update() {
+    this.items.forEach(item =>
+     item.count ++
+    )
   }
 
   ngOnInit() {
-    console.log('hello `Home` component');
-    // this.title.getData().subscribe(data => this.data = data);
-  }
+      this._dashboardService.getDashboardItems()
+          .subscribe(res => {
+              this.items = res.homeNav;
+          });
 
-  submitState(value: string) {
-    console.log('submitState', value);
-    this.appState.set('value', value);
-    this.localState.value = '';
+      setInterval(() => this.update(), 10000);
   }
 }
